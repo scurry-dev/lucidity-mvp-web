918deb4736e8130b202a104dac43e43e0f512406
lucidityanalytics <71618893+lucidityanalytics@users.noreply.github.com>
2025-09-29 11:41:24 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: mode === 'production' ? '/privacy-check-bot/' : '/',
+  base: '/',
 }));
